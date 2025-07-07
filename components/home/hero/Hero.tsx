316be0c3865d8f9be7f1c4cd23fd42a8a081b76a
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2025-07-07 09:11:31 +0530

    Update Hero.tsx
    error fix in hero section

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -27,7 +27,7 @@ export const Hero = () => {
           <Reveal>
             <p className={styles.aboutCopy}>
               I&apos:m a web developer with a strong foundation in Computer Science and a deep interest in AI/ML. Over the past few years, I've built impactful web applications through personal projects and internships, constantly expanding my skill set. Passionate about creating modern digital solutions—always curious, always growing.
-              <br>Let&apos;s connect!</br>
+              <br />Let&apos;s connect!
             </p>
           </Reveal>
 
